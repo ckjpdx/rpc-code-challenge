e40e3ed7fc9b521a759721a483fa835d8ebc332b
Chris Knight Johnson <cknightjohnson@gmail.com>
2022-04-10 12:40:20 -0700

    create utils scss module

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import utils from '../styles/utils.module.scss'
 // @ts-check
 
 const Home: NextPage = () => {
